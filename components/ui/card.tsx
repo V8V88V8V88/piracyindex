6c9df3020241d2e21281cbe489a7973f33cd7525
Vaibhav Pratap Singh <v8v88v8v88@protonmail.com>
2025-01-01 23:35:37 +0530

    few theme changes

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -10,7 +10,8 @@ const Card = React.forwardRef<
     ref={ref}
     className={cn(
       "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className
+      className,
+      "bg-[#0b0d10]"
     )}
     {...props}
   />
