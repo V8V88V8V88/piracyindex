77d9b1b69e9e28dfd4073e6216542deb33b9c1bf
Vaibhav Pratap Singh <v8v88v8v88@protonmail.com>
2025-01-03 23:49:22 +0530

    changed the hieght and centered the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function Footer() {
   return (
     <footer className="py-6 px-4 md:px-8">
-      <div className="container flex items-center justify-center h-24">
+      <div className="container flex items-center justify-center h-2">
         <p className="text-center text-sm leading-loose text-muted-foreground">
           This site is a love creation of{' '}
           <a
